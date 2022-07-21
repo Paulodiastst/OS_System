ba100c4abc93ba55446927821e6fa77d49d26184
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-07-20 21:44:01 -0300

    Endpoint da classe técnico

---
+++ src/main/java/com/paulo/os/services/TecnicoService.java
@@ -0,0 +1,22 @@
+package com.paulo.os.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulo.os.domain.Tecnico;
+import com.paulo.os.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository repository;
+
+	public Tecnico findById(Integer Id) {
+		Optional<Tecnico> obj = repository.findById(Id);
+		return obj.orElse(null);
+	}
+
+}
