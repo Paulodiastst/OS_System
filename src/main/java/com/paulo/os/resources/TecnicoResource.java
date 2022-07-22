87820ec7815e5b3373d02ee16dd6ed749d339f57
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-07-22 13:39:16 -0300

    Update Resource

--- src/main/java/com/paulo/os/resources/TecnicoResource.java
+++ src/main/java/com/paulo/os/resources/TecnicoResource.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.paulo.os.domain.Tecnico;
 import com.paulo.os.dtos.TecnicoDTO;
 import com.paulo.os.services.TecnicoService;
 
