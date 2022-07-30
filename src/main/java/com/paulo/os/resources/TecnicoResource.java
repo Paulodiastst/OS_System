f36623faa9704df583a4dc513bb659b3867c87e1
Paulo Roberto <paulodiastst@hotmail.com>
2022-07-30 00:45:16 -0300

    POST Tecnico

--- src/main/java/com/paulo/os/resources/TecnicoResource.java
+++ src/main/java/com/paulo/os/resources/TecnicoResource.java
@@ -1,5 +1,6 @@
 package com.paulo.os.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.paulo.os.domain.Tecnico;
 import com.paulo.os.dtos.TecnicoDTO;
 import com.paulo.os.services.TecnicoService;
 
@@ -32,5 +37,13 @@ public class TecnicoResource {
 				.collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDto) {
+		Tecnico newObj = service.create(objDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(newObj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
