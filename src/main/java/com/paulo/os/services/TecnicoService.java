70a9d5ac7a9aeaf8061d2aa1025fb8bb6f9bdf1f
Paulo Roberto <paulodiastst@hotmail.com>
2022-08-08 13:07:48 -0300

    PUT Tecnico

--- src/main/java/com/paulo/os/services/TecnicoService.java
+++ src/main/java/com/paulo/os/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.paulo.os.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,15 +31,29 @@ public class TecnicoService {
 	}
 
 	public Tecnico create(TecnicoDTO objDTO) {
-		if(findByCPF(objDTO) != null){
-			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!"); 
+		if (findByCPF(objDTO) != null) {
+			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
 		}
-		
+
 		return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
+	}
+
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		Tecnico oldObj = findById(id);
+
+		if (findByCPF(objDTO) != null && findByCPF(objDTO).getId() != id) {
+			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+		}
+
+		oldObj.setNome(objDTO.getNome());
+		oldObj.setCpf(objDTO.getCpf());
+		oldObj.setTelefone(objDTO.getTelefone());
+		return repository.save(oldObj);
 	}
 
 	private Tecnico findByCPF(TecnicoDTO objDTO) {
 		Tecnico obj = repository.findByCPF(objDTO.getCpf());
+		
 		if (obj != null) {
 			return obj;
 		}
